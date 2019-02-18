7991d7cdcd222ff830e40edb84502cfdc9360e72
Stefan <shodgeskluck@gmail.com>
2019-02-18 14:11:45 -0500

    Removed task items from popup text

--- src/components/task-info/TaskInfo.js
+++ src/components/task-info/TaskInfo.js
@@ -1,10 +1,9 @@
 import React from 'react'
 
-export default function TaskInfo({ location, text }) {
+export default function TaskInfo({ location }) {
   return (    
     <div>
       <h3>{ location.toUpperCase() }</h3>
-      <p><strong>Task:</strong> { text }</p>
     </div>
   )
 }
